660e5c7ba1f6b7588f78c478b96c2a844343c4aa
bintangazharn <bintangazhar.n@gmail.com>
2022-01-23 17:48:53 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+?>
+
+<HTML>
+    <head>
+        <title>Workshop Electro Polinema</title>
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+        <link href="./assets/styling/main.css" rel="stylesheet"/>
+        <link href="./assets/fonts/stylesheet.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="header">
+            <div class="header_left">
+                <div class="menu_container">
+                    <div class="img_menu">
+                        <img width="70px" src="./assets/images/vector_ws.svg"/>
+                    </div>
+                    <div class="menu">
+                        <ul>
+                            <li><a href="#">Cerita</a></li>
+                            <li><a href="#">Layanan</a></li>
+                            <li><a href="#">Tim</a></li>
+                            <li><a href="#">Hubungi Kami</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="title_container">
+                    <h1>Kembangkan<br/>Proyekmu bersama<br/>Workshop Electro</h1>
+                    <a href="#">PELAJARI SELENGKAPNYA</a>
+                </div>
+                <div class="header_content_container">
+                    <h6>DIPERCAYA DI BERBAGAI INDUSTRI</h6>
+                    <p>Dengan track record pengerjaan project yang selalu selesai tepat waktu
+                        <br/>dengan sangat baik membuat WSEC Polinema dipercaya oleh berbagai
+                        <br/>industri multinasional.</p>
+                </div>
+            </div>
+            <div class="header_right">
+                &nbsp;
+            </div>
+        </div>
+        <div class="content">
+            <div class="content_cerita">
+                <div class="content_cerita_left">
+                    <h1>Kisah Awal Kami</h1>
+                    <p>Kami berawal dari riset dan pengembangan Divisi IT
+                        <br/>yang menghasilkan output jasa pembuatan website.</p>
+                </div>
+                <div class="content_cerita_right">
+                    <iframe width="400" height="285.71" src="https://www.youtube.com/embed/LUSX6fYcBpQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="content_harga">
+                <h1>Paket Layanan Kami</h1>
+                <p>Kami mempunyai beberapa paket pembuatan website dengan berbagai pilihan dan
+                    <br/>keuntungan yang banyak.</p>
+                <div class="content_harga_main">
+                    <div class="content_harga_main_1">
+                        <h6>LANDING PAGE</h6>
+                        <h1><blockquote>RP</blockquote>499K</h1>
+                        <p>Cocok bagi perusahaan Anda
+                            <br/>yang ingin membuat website
+                            <br/>untuk memperkenalkan
+                            <br/>identitas perusahaan Anda.</p>
+                        <a href="#">ORDER</a><br/>
+                    </div>
+                    <div class="content_harga_main_2">
+                        <h6>ONLINE SHOP</h6>
+                        <h1><blockquote>RP</blockquote>799K</h1>
+                        <p>Cocok bagi Anda yang
+                            <br/>mempunyai usaha dan ingin
+                            <br/>memperluas jangkauan bisnis
+                            <br/>melalui online shop.</p>
+                        <a href="#">ORDER</a><br/>
+                    </div>
+                    <div class="content_harga_main_3">
+                        <h6>PORTOFOLIO</h6>
+                        <h1><blockquote>RP</blockquote>899K</h1>
+                        <p>Cocok bagi perusahaan Anda
+                            <br/>yang ingin membangun
+                            <br/>kepercayaan publik dengan<br/>menunjukkan portofolio proyek.</p>
+                        <a href="#">ORDER</a><br/>
+                    </div>
+                </div>
+            </div>
+<!--            <div class="content_testimoni">-->
+<!---->
+<!--            </div>-->
+<!--            <div class="content_team">-->
+<!---->
+<!--            </div>-->
+<!--            <div class="content_contact"-->
+        </div>
+        <div class="footer">
+            <div class="upper_footer">
+                <div class="footer_logo">
+                    <img width="70px" src="./assets/images/vector_ws.svg"/>
+                </div>
+                <div class="footer_content">
+                    <p>
+                        Dengan asas organisasi yang bersifat kekeluargaan membuat jaringan kerjasama kami
+                        <br/>sangat luas dengan organisasi sejenis dari universitas maupun politkenik sahabat.
+                    </p>
+                    <br/>
+                    <i class="ri-slack-fill"></i><i class="ri-linkedin-fill"></i><i class="ri-youtube-fill"></i> <i class="ri-instagram-line"></i>
+                </div>
+            </div>
+            <div class="bottom_footer">
+                <p>© 2022 Workshop Electro - Malang, Indonesia</p>
+            </div>
+        </div>
+    </body>
+</HTML>
